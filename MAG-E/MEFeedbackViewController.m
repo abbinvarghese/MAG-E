c801363b2b54f851436d1e7fa98213205d26ea16
AbbinVarghese <paadam2@gmail.com>
2016-06-14 16:47:07 +0530

    Comm

--- MAG-E/MEFeedbackViewController.m
+++ MAG-E/MEFeedbackViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationController.navigationBar.tintColor = [UIColor blackColor];
     // Do any additional setup after loading the view.
 }
 
